ea756c51c27d3803584eb61ae3ec3ec9e6459c9b
Deka <dekatruong@gmail.com>
2016-05-08 04:20:47 +0700

    - Alpha version: improve example

--- example/app/controllers/IndexController.php
+++ example/app/controllers/IndexController.php
@@ -9,7 +9,7 @@ use Phalcon\Mvc\Controller;
 class IndexController extends Controller {
 
     public function indexAction() {
-        echo 'PhalconBoot has worked sucessfully!';
+        
     }
         
 }
